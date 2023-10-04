a99cbf8c394ebaa74c0152630a83236006cbce31
brightgeevarghese <brightlsom@gmail.com>
2023-10-04 11:02:32 -0500

    modified java version in pom

---
+++ src/main/java/edu/miu/ce/cs489/App.java
@@ -0,0 +1,13 @@
+package edu.miu.ce.cs489;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
